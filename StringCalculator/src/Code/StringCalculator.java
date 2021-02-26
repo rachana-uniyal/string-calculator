03a52499a35f6cd0592fbf78d8838035219116b3
Rachana Uniyal <rachanauniyal0417@gmail.com>
2021-02-26 22:07:41 +0530

    test done

--- StringCalculator/src/Code/StringCalculator.java
+++ StringCalculator/src/Code/StringCalculator.java
@@ -4,6 +4,7 @@ public class StringCalculator {
 
 
     public int add(String s) {
+        s ="1,2";
         int result=0;
         if(s.isEmpty()) {
             result = 0;
@@ -11,6 +12,14 @@ public class StringCalculator {
         else{
             if(s.length()==1)
                 result = Integer.parseInt(s);
+            else{
+                String[] text = s.split(",");
+                Integer x=0;
+                for (int i = 0; i<text.length ; i++){
+                     x = Integer.valueOf(text[i]);
+                     result+=x;
+                }
+            }
         }
      return result;
     }
